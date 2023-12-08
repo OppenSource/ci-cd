d11d6e507d028c10cc7cf3a563e7c77e1184480e
OppenSource <brice.nanyang@institutsaintjean.org>
2023-12-09 00:26:40 +0100

    ajout du cv et des Ajout d'un test

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
-const changeMessage = () => {
-  document.getElementById("message").textContent = "Message changé!";
-};
+// script.js
 
+// Function to change the content of the 'message' element
+function changeMessage() {
+  const messageElement = document.getElementById("message");
+
+  if (messageElement) {
+    messageElement.innerHTML = "Download CV now!";
+  } else {
+    console.error("Element with ID 'message' not found.");
+  }
+}
+
+// Call the changeMessage function
 module.exports = { changeMessage };
--- tests/script.test.js
+++ tests/script.test.js
@@ -1,15 +1,28 @@
+// script.test.js
+const fs = require("fs");
+const path = require("path");
 const { JSDOM } = require("jsdom");
+
+// Load the HTML file using JSDOM
+const html = fs.readFileSync(path.resolve(__dirname, "../index.html"), "utf-8");
+const { window } = new JSDOM(html);
+global.document = window.document;
+
+// Load the script file
 const script = require("../js/script.js");
 
-const dom = new JSDOM(
-  `<!DOCTYPE html><html><body><p id="message">Bonjour le monde!</p></body></html>`
-);
-global.document = dom.window.document;
+describe("changeMessage function", () => {
+  it("should change the content of the message element", () => {
+    // Create an element with the ID 'message'
+    const messageElement = document.createElement("div");
+    messageElement.id = "message";
+    document.body.appendChild(messageElement);
 
-describe("Hello World Function", () => {
-  it("should change the message", () => {
-    const message = document.getElementById("message");
+    // Call the changeMessage function
     script.changeMessage();
-    expect(message.textContent).toBe("Message changé!");
+
+    // Check if the content has been changed
+    expect(messageElement.innerHTML).toBe("Download CV now!");
   });
+
 });
